a1674380e03df5600a5139fcca2b17ee75f8507d
Yavuz Selim KAHRAMAN <yskahraman@gmail.com>
2023-11-27 21:41:17 +0300

    completd

---
+++ FullStack.WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,138 @@
+﻿using FullStack.WebAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace FullStack.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        // localhost : ???? /api/Products
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // Success Status Code 200...
+
+            //response.IsSuccessStatusCode
+
+            // 200 OK 201 Created 204 NoContent 400 BadRequest 404 NotFound 500 Problem
+            // 
+
+
+            // => Veri çekerken  bu işlem başarılı ise => 200
+            // => Eklerken => bu işlem başarılı => 201 (geri dönersiniz)
+            // => Silme => silme işlemi başarılı ise => 204 
+            // => Update => update işlemi başarıl ise => 204 
+
+
+            // İşlem başarıszsa ? 
+            // Veriyi bulamadık => 404 => NotFound
+            // Atılan request istedğimiz gibi değil => 400 Bad Request
+            // UnAuthorized =>  401 | 403
+            // Database patladı, connection pool şişti vs vs. 500 => Problem
+
+
+            // V8 Engine + Javacsript 
+            // npm => node package manager
+            // Node.js  || 
+
+            throw new Exception("Bir hata oluştu");
+
+            return Ok(ProductService.products);
+
+            //return BadRequest();
+            //return Ok(new { Id = 1, Name = "Iphone 15" });
+
+        }
+
+        //[HttpGet("GetProductsByCategory/{categoryId}")]
+        //public IActionResult GetProductsByCategoryId(int categoryId)
+        //{
+        //    var products = ProductService.products.Where(x => x.CategoryId == categoryId).ToList();
+
+        //    foreach (var product in products)
+        //    {
+        //        product.Category = ProductService.categories.SingleOrDefault(x => x.Id == product.CategoryId) ?? new Category();
+        //    }
+
+        //    return Ok(products);
+        //}
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+
+            var product = ProductService.products.SingleOrDefault(x => x.Id == id);
+
+            if (product == null)
+            {
+                return NotFound(new
+                {
+                    Message = "Product not found",
+                    Value = id,
+                });
+            }
+
+
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult Create(string product)
+        {
+            if (product == null)
+            {
+                return BadRequest();
+            }
+
+            var createdProduct = ProductService.Create(product);
+            return Created(string.Empty, createdProduct);
+        }
+
+        [HttpPut]
+        public IActionResult Update(Product product)
+        {
+            if (product == null)
+            {
+                return BadRequest();
+            }
+            ProductService.Update(product);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            ProductService.Delete(id);
+            return NoContent();
+        }
+
+        [HttpGet("GetFile")]
+        public IActionResult GetFile()
+        {
+            string data = "text file dan gelen data";
+            return File(Encoding.UTF8.GetBytes(data), "application/text");
+        }
+
+    }
+
+    //public class TestController : Controller
+    //{
+
+    //}
+
+
+    /* 
+     MERN MEVN 
+     
+    M=> Mongo
+    E => Express.js
+    R => React.js  V => Vue  A => Angular
+    N => Node.js
+
+     */
+}
---
+++ FullStack.WebAPI/Data/Product.cs
@@ -0,0 +1,62 @@
+﻿namespace FullStack.WebAPI.Data
+{
+    public class ProductService
+    {
+        public static List<Product> products = new()
+        {
+            new Product{ Id = 1, Name="Ipone 15"},
+            new Product{ Id = 2, Name ="Macbook Pro"},
+            new Product{ Id = 3, Name = "Samsung S21 5g"},
+            new Product{Id = 4, Name="Bluz"}
+        };
+
+        //public static List<Category> categories = new List<Category>()
+        //{
+        //    new Category{ Id = 1, Name ="Elektronik"},
+        //    new Category{ Id = 2, Name ="Giyim"}
+        //};
+
+        public static Product Create(string name)
+        {
+            var id = products.Last().Id + 1;
+            var product = new Product
+            {
+                Id = id,
+                Name = name,
+            };
+            products.Add(product);
+            return product;
+        }
+
+        public static void Update(Product product)
+        {
+            var updatedProduct = products.SingleOrDefault(x => x.Id == product.Id);
+            if (updatedProduct != null)
+            {
+                updatedProduct.Name = product.Name;
+            }
+        }
+
+        public static void Delete(int id)
+        {
+            var removedProduct = products.SingleOrDefault(x => x.Id == id);
+            if (removedProduct != null)
+            {
+                products.Remove(removedProduct);
+            }
+
+        }
+
+    }
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+    }
+}
